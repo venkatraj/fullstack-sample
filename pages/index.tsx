dfec4f605697c1647c9cc031cc80014bd3cb5bd7
unknown <venkat@codinggeek.com>
2020-08-07 11:20:30 +0530

    use graphql client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import AllUsers from '../components/AllUsers';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
@@ -9,7 +10,8 @@ const IndexPage = () => (
         <a>About</a>
       </Link>
     </p>
+    <AllUsers />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
